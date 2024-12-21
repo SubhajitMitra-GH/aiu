9761604070553345a749354a0d61b503c34fe549
Subhajit Mitra <msubhojit182004@gmail.com>
2024-12-21 18:04:31 +0530

    open

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -34,6 +34,7 @@ const Home = () => {
 
   const handleDownload = (pdfPath) => {
     if (pdfPath) {
+      window.open(pdfPath, "_blank");
       const link = document.createElement("a");
       link.href = pdfPath;
       link.download = pdfPath.split("/").pop();
